cec3924c6288588677f3a512e1a2610439a04618
BenLooper <benlooper08@gmail.com>
2020-11-02 17:26:06 -0600

    Done.

--- README.md
+++ README.md
@@ -18,4 +18,13 @@ After the stocks are fetched, your job is to:
 * allow a user to sort the list of stocks alphabetically by the ticker name as well as by ascending price.
 * allow a user to filter stocks based on the type of the stock.
 
+“Core deliverables” for practice challenge today:
+Render all the stocks onto the page. The styling of how a Stock should look like is already in the Stock.js component.
+allow a user to buy a stock by clicking on it and when it is bought, it should be added to My Portfolio.
+allow a user to sell a stock in their Portfolio by clicking on the stock and it should be removed from their Portfolio.
+Add a search bar to the project (similar to Pokémon lab)
+BONUS:
+allow a user to sort the list of stocks alphabetically by the ticker name as well as by ascending price.
+allow a user to filter stocks based on the type of the stock.
+
 Best of luck!
--- src/App.js
+++ src/App.js
@@ -3,11 +3,101 @@ import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = ({
+      stocks: [],
+      myStocks: [],
+      checked:'',
+      filter:''
+    })
+  }
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res=>res.json())
+    .then(stocks => this.setState({stocks}))
+  }
+
+  buyStock = (selectedStock) => {
+    this.setState({
+      myStocks: [...this.state.myStocks, selectedStock]
+    })
+  }
+
+  sellStock = (selectedStock) => {
+    this.setState({
+      myStocks: this.state.myStocks.filter(stock => stock.id !== selectedStock.id)
+    })
+  }
+
+  searchName = (e) => {
+    let userInput = e.target.value.toLowerCase()
+    let stocks = this.state.stocks.sort(function(a,b){
+      let nameA =  a.name.toLowerCase(); 
+
+      let nameB = b.name.toLowerCase();
+
+      if (!nameA.includes(userInput) && nameB.includes(userInput)){
+        return 1;
+      }
+      else if (nameA.includes(userInput) && !nameB.includes(userInput)){
+        return -1;
+      }
+      else{
+        return 0;
+      }
+    })
+    this.setState({
+      stocks: stocks 
+    })
+  }
+
+  sortBy = (e) => {
+    let category = e.target.value
+    switch(category){
+      case 'Alphabetically':
+        this.setState({
+          checked: "a",
+          stocks: this.state.stocks.sort((a,b) => (a.name > b.name) ? 1:-1)
+        })
+        break;
+      case 'Price':
+        this.setState({
+          checked: "p",
+          stocks: this.state.stocks.sort((a,b) => (a.price > b.price) ? 1:-1)
+        })
+        break;
+      default:
+        console.log(category) 
+    }
+  }
+
+  filterStocks = (e) => {
+    this.setState({
+      filter: e.target.value
+    })
+  }
+
   render() {
+    const stocks = this.state.stocks.filter(stock => stock.type.includes(this.state.filter))
     return (
       <div>
         <Header/>
-        <MainContainer/>
+
+        <MainContainer 
+        stocks={stocks}
+        myStocks={this.state.myStocks}
+        buyStock={this.buyStock}
+        sellStock={this.sellStock}
+        searchName={this.searchName}
+        sortBy={this.sortBy}
+        checked={this.state.checked}
+        filterStocks={this.filterStocks}
+        />
+
       </div>
     );
   }
