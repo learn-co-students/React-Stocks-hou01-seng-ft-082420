5fcb3b0aff52b5ed7885f0b0138ab68235872ec6
Your Name <you@example.com>
2020-11-02 16:00:09 -0600

    Dane full deliverables solution

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
+      <label>
+        <strong>Search Stocks</strong>
+        <input onChange={(e)=>props.changeSearchTerm(e)}/>
+      </label>
+      <br/>
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sort === 'Alphabetically'} onChange={(e)=>props.updateSort(e.target.value)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sort === 'Price'} onChange={(e)=>props.updateSort(e.target.value)}/>
         Price
+      </label>
+      <label>
+        <input type="radio" value="Default" checked={props.sort === 'Default'} onChange={(e)=>props.updateSort(e.target.value)}/>
+        Default
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e)=>props.updateFilter(e.target.value)}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,23 +2,89 @@ import React, { Component } from 'react';
 import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
-
+let stockUrl = 'http://localhost:3000/stocks'
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    myStocks: [],
+    filter: 'All',
+    sort: 'None',
+    searchTerm: ""
+  }
 
+  componentDidMount = () => {
+    fetch(stockUrl)
+    .then(res=>res.json())
+    .then(stocks=> this.setState({stocks}))
+  }
+
+  addStock = (addedStock) => {
+    if (!this.state.myStocks.find(stock => stock === addedStock)){
+      this.setState({
+        myStocks: [...this.state.myStocks, addedStock]
+      })
+    }
+  }
+
+  removeStock = (removedStock) => {
+    this.setState({
+      myStocks: this.state.myStocks.filter(stock=> stock != removedStock)
+    })
+  }
+
+  changeSearchTerm = (e) => {
+    this.setState({searchTerm: e.target.value})
+  }
+
+  displayStocks = () => {
+    let displayStocks = this.state.stocks.filter(stock=>stock.name.toLowerCase().includes(this.state.searchTerm))
+    if(this.state.filter !== "All"){
+      displayStocks = displayStocks.filter(stock=>stock.type == this.state.filter)
+    }
+
+    switch(this.state.sort){
+      case "Alphabetically":
+        return displayStocks.sort((a,b)=> a.name > b.name ? 1 : -1)
+      case "Price":
+        return displayStocks.sort((a,b)=> a.price > b.price ? 1 : -1)
+      default:
+          return displayStocks
+    }
+
+  }
+
+  updateFilter = (filter) => {
+    this.setState({filter})
+  }
+
+  updateSort = (sort) => {
+    this.setState({sort})
+  }
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          sort={this.state.sort}
+          updateSort={this.updateSort}
+          updateFilter={this.updateFilter}
+          changeSearchTerm={this.changeSearchTerm}
+        />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer
+                addStock= {this.addStock}
+                stocks = {this.displayStocks()}
+              />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer
+              removeStock = {this.removeStock}
+                stocks={this.state.myStocks}
+              />
 
             </div>
           </div>
